c7bc2539942e8e48369fe232f9619c905ff46824
MHZDeveloper <mohamed.hichem.zayani@gmail.com>
2019-02-11 16:41:58 +0100

    First Commit

---
+++ src/main/java/com/test/besttrip/BesttripApplication.java
@@ -0,0 +1,14 @@
+package com.test.besttrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BesttripApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BesttripApplication.class, args);
+	}
+
+}
+
